84f2ad03cd5b32dcf78fd0bb89da832ecbb04507
romancis <robotikmansacia46@gmail.com>
2026-06-14 16:36:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,33 @@
+// =========================
+// FIREBASE CONFIG
+// =========================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+// Konfigurasi Firebase project kamu
+const firebaseConfig = {
+  apiKey: "AIzaSyAcYGCGQcwaFuXxd7R4Oq-hJRgaBRYuRPM",
+  authDomain: "romancis--smarthome.firebaseapp.com",
+  projectId: "romancis--smarthome",
+  storageBucket: "romancis--smarthome.firebasestorage.app",
+  messagingSenderId: "542536848118",
+  appId: "1:542536848118:web:059f833254c14880ff92d7",
+  measurementId: "G-QLK7DPFCXD"
+};
+
+// =========================
+// INIT FIREBASE APP
+// =========================
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Debug
+console.log("Firebase Connected");
+
+// =========================
+// EXPORT
+// =========================
+export { db };
